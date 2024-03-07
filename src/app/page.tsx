4953b860b8596964d7cfea5245e3a19f6ac1f226
gustavodev200 <gustavolage200@gmail.com>
2024-03-06 22:27:13 -0300

    feat: added timing at page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
 import { PomodoroStatusIndicator } from "./components";
 
 export default function Home() {
   return (
-    <main className="flex w-full h-screen bg-red-50 items-center justify-center">
+    <main className="flex w-full h-screen bg-red-50 items-center justify-center flex-col">
       <PomodoroStatusIndicator />
+      <div className=" text-red-900 ">
+        <span className="flex flex-col  text-[256px]  font-light">
+          25<span className="-mt-40">00</span>
+        </span>
+      </div>
     </main>
   );
 }
